3be8ef096f957ea25845975229dffc10bd1cd859
Fabio <fabiodsilva@yahoo.com.br>
2022-06-17 10:58:22 -0300

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 #  profile = "lab"
 }
 
-#variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_ACCESS_KEY_ID" {}
 
-#variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 
 
 
